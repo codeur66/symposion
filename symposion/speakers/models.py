codeur66/symposion

commit 36ab6d599ffca01b83d5a93a2b5b7fa6b5bd9d70

Fix flake8 warnings

symposion/speakers/models.py
@@ -9,44 +9,47 @@ from markitup.fields import MarkupField
 
 
 class Speaker(models.Model):
-    
+
     SESSION_COUNT_CHOICES = [
         (1, "One"),
         (2, "Two")
     ]
-    
+
     user = models.OneToOneField(User, null=True, related_name="speaker_profile")
-    name = models.CharField(max_length=100, help_text="As you would like it to appear in the conference program.")
-    biography = MarkupField(blank=True, help_text="A little bit about you. Edit using <a href='http://warpedvisions.org/projects/markdown-cheat-sheet/' target='_blank'>Markdown</a>.")
+    name = models.CharField(max_length=100, help_text=("As you would like it to appear in the "
+                                                       "conference program."))
+    biography = MarkupField(blank=True, help_text=("A little bit about you.  Edit using "
+                                                   "<a href='http://warpedvisions.org/projects/"
+                                                   "markdown-cheat-sheet/target='_blank'>"
+                                                   "Markdown</a>."))
     photo = models.ImageField(upload_to="speaker_photos", blank=True)
     annotation = models.TextField()  # staff only
     invite_email = models.CharField(max_length=200, unique=True, null=True, db_index=True)
     invite_token = models.CharField(max_length=40, db_index=True)
     created = models.DateTimeField(
-        default = datetime.datetime.now,
-        editable = False
+        default=datetime.datetime.now,
+        editable=False
     )
 
     class Meta:
         ordering = ['name']
-    
+
     def __unicode__(self):
         if self.user:
             return self.name
         else:
             return "?"
 
-    
     def get_absolute_url(self):
         return reverse("speaker_edit")
-    
+
     @property
     def email(self):
         if self.user is not None:
             return self.user.email
         else:
             return self.invite_email
-    
+
     @property
     def all_presentations(self):
         presentations = []

symposion/boxes/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.db import models
 
 from django.contrib.auth.models import User
@@ -10,16 +8,16 @@ from markitup.fields import MarkupField
 
 
 class Box(models.Model):
-    
+
     label = models.CharField(max_length=100, db_index=True)
     content = MarkupField(blank=True)
-    
+
     created_by = models.ForeignKey(User, related_name="boxes")
     last_updated_by = models.ForeignKey(User, related_name="updated_boxes")
-    
+
     def __unicode__(self):
         return self.label
-    
+
     class Meta:
         verbose_name_plural = "boxes"
 

symposion/cms/models.py
@@ -18,12 +18,12 @@ from .managers import PublishedPageManager
 
 
 class Page(models.Model):
-    
+
     STATUS_CHOICES = (
         (1, _("Draft")),
         (2, _("Public")),
     )
-    
+
     title = models.CharField(max_length=100)
     path = models.CharField(max_length=100, unique=True)
     body = MarkupField()
@@ -32,28 +32,29 @@ class Page(models.Model):
     created = models.DateTimeField(editable=False, default=datetime.datetime.now)
     updated = models.DateTimeField(editable=False, default=datetime.datetime.now)
     tags = TaggableManager(blank=True)
-    
+
     published = PublishedPageManager()
-    
+
     def __unicode__(self):
         return self.title
-    
+
     @models.permalink
     def get_absolute_url(self):
         return ("cms_page", [self.path])
-    
+
     @property
     def is_community(self):
         return self.path.lower().startswith("community/")
-    
+
     def save(self, *args, **kwargs):
         self.updated = datetime.datetime.now()
         super(Page, self).save(*args, **kwargs)
-    
+
     def clean_fields(self, exclude=None):
         super(Page, self).clean_fields(exclude)
         if not re.match(settings.SYMPOSION_PAGE_REGEX, self.path):
-            raise ValidationError({"path": [_("Path can only contain letters, numbers and hyphens and end with /")]})
+            raise ValidationError(
+                {"path": [_("Path can only contain letters, numbers and hyphens and end with /")]})
 
 
 reversion.register(Page)
@@ -64,9 +65,9 @@ def generate_filename(instance, filename):
 
 
 class File(models.Model):
-    
+
     file = models.FileField(upload_to=generate_filename)
     created = models.DateTimeField(default=datetime.datetime.now)
-    
+
     def download_url(self):
         return reverse("file_download", args=[self.pk, os.path.basename(self.file.name).lower()])

symposion/conference/models.py
@@ -11,24 +11,24 @@ class Conference(models.Model):
     """
     the full conference for a specific year, e.g. US PyCon 2012.
     """
-    
+
     title = models.CharField(_("title"), max_length=100)
-    
+
     # when the conference runs
     start_date = models.DateField(_("start date"), null=True, blank=True)
     end_date = models.DateField(_("end date"), null=True, blank=True)
-    
+
     # timezone the conference is in
     timezone = TimeZoneField(_("timezone"), blank=True)
-    
+
     def __unicode__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         super(Conference, self).save(*args, **kwargs)
         if self.id in CONFERENCE_CACHE:
             del CONFERENCE_CACHE[self.id]
-    
+
     def delete(self):
         pk = self.pk
         super(Conference, self).delete()
@@ -36,7 +36,7 @@ class Conference(models.Model):
             del CONFERENCE_CACHE[pk]
         except KeyError:
             pass
-    
+
     class Meta(object):
         verbose_name = _("conference")
         verbose_name_plural = _("conferences")
@@ -48,19 +48,19 @@ class Section(models.Model):
     "Talks", "Expo", "Sprints", that may have its own review and
     scheduling process.
     """
-    
+
     conference = models.ForeignKey(Conference, verbose_name=_("conference"))
-    
+
     name = models.CharField(_("name"), max_length=100)
     slug = models.SlugField()
 
     # when the section runs
     start_date = models.DateField(_("start date"), null=True, blank=True)
     end_date = models.DateField(_("end date"), null=True, blank=True)
-    
+
     def __unicode__(self):
         return "%s %s" % (self.conference, self.name)
-    
+
     class Meta(object):
         verbose_name = _("section")
         verbose_name_plural = _("sections")

symposion/proposals/models.py
@@ -21,20 +21,20 @@ from symposion.conference.models import Section
 class ProposalSection(models.Model):
     """
     configuration of proposal submissions for a specific Section.
-    
+
     a section is available for proposals iff:
       * it is after start (if there is one) and
       * it is before end (if there is one) and
       * closed is NULL or False
     """
-    
+
     section = models.OneToOneField(Section)
-    
+
     start = models.DateTimeField(null=True, blank=True)
     end = models.DateTimeField(null=True, blank=True)
     closed = models.NullBooleanField()
     published = models.NullBooleanField()
-    
+
     @classmethod
     def available(cls):
         now = datetime.datetime.now()
@@ -43,7 +43,7 @@ class ProposalSection(models.Model):
             Q(end__gt=now) | Q(end=None),
             Q(closed=False) | Q(closed=None),
         )
-    
+
     def is_available(self):
         if self.closed:
             return False
@@ -53,7 +53,7 @@ class ProposalSection(models.Model):
         if self.end and self.end < now:
             return False
         return True
-    
+
     def __unicode__(self):
         return self.section.name
 
@@ -61,68 +61,77 @@ class ProposalSection(models.Model):
 class ProposalKind(models.Model):
     """
     e.g. talk vs panel vs tutorial vs poster
-    
+
     Note that if you have different deadlines, reviewers, etc. you'll want
     to distinguish the section as well as the kind.
     """
-    
+
     section = models.ForeignKey(Section, related_name="proposal_kinds")
-    
+
     name = models.CharField(_("Name"), max_length=100)
     slug = models.SlugField()
-    
+
     def __unicode__(self):
         return self.name
 
 
 class ProposalBase(models.Model):
-    
+
     objects = InheritanceManager()
-    
+
     kind = models.ForeignKey(ProposalKind)
-    
+
     title = models.CharField(max_length=100)
     description = models.TextField(
         _("Brief Description"),
         max_length=400,  # @@@ need to enforce 400 in UI
-        help_text="If your proposal is accepted this will be made public and printed in the program. Should be one paragraph, maximum 400 characters."
+        help_text=_("If your proposal is accepted this will be made public and printed in the "
+                    "program. Should be one paragraph, maximum 400 characters.")
     )
     abstract = MarkupField(
         _("Detailed Abstract"),
-        help_text=_("Detailed outline. Will be made public if your proposal is accepted. Edit using <a href='http://daringfireball.net/projects/markdown/basics' target='_blank'>Markdown</a>.")
+        help_text=_("Detailed outline. Will be made public if your proposal is accepted. Edit "
+                    "using <a href='http://daringfireball.net/projects/markdown/basics' "
+                    "target='_blank'>Markdown</a>.")
     )
     additional_notes = MarkupField(
         blank=True,
-        help_text=_("Anything else you'd like the program committee to know when making their selection: your past experience, etc. This is not made public. Edit using <a href='http://daringfireball.net/projects/markdown/basics' target='_blank'>Markdown</a>.")
+        help_text=_("Anything else you'd like the program committee to know when making their "
+                    "selection: your past experience, etc. This is not made public. Edit using "
+                    "<a href='http://daringfireball.net/projects/markdown/basics' "
+                    "target='_blank'>Markdown</a>.")
     )
     submitted = models.DateTimeField(
         default=datetime.datetime.now,
         editable=False,
     )
     speaker = models.ForeignKey("speakers.Speaker", related_name="proposals")
-    additional_speakers = models.ManyToManyField("speakers.Speaker", through="AdditionalSpeaker", blank=True)
+    additional_speakers = models.ManyToManyField("speakers.Speaker", through="AdditionalSpeaker",
+                                                 blank=True)
     cancelled = models.BooleanField(default=False)
-    
+
     def can_edit(self):
         return True
-    
+
     @property
     def section(self):
         return self.kind.section
-    
+
     @property
     def speaker_email(self):
         return self.speaker.email
-    
+
     @property
     def number(self):
         return str(self.pk).zfill(3)
-    
+
     def speakers(self):
         yield self.speaker
-        for speaker in self.additional_speakers.exclude(additionalspeaker__status=AdditionalSpeaker.SPEAKING_STATUS_DECLINED):
+        speakers = self.additional_speakers.exclude(
+            additionalspeaker__status=AdditionalSpeaker.SPEAKING_STATUS_DECLINED)
+        for speaker in speakers:
             yield speaker
-    
+
     def notification_email_context(self):
         return {
             "title": self.title,
@@ -135,21 +144,21 @@ reversion.register(ProposalBase)
 
 
 class AdditionalSpeaker(models.Model):
-    
+
     SPEAKING_STATUS_PENDING = 1
     SPEAKING_STATUS_ACCEPTED = 2
     SPEAKING_STATUS_DECLINED = 3
-    
+
     SPEAKING_STATUS = [
         (SPEAKING_STATUS_PENDING, _("Pending")),
         (SPEAKING_STATUS_ACCEPTED, _("Accepted")),
         (SPEAKING_STATUS_DECLINED, _("Declined")),
     ]
-    
+
     speaker = models.ForeignKey("speakers.Speaker")
     proposalbase = models.ForeignKey(ProposalBase)
     status = models.IntegerField(choices=SPEAKING_STATUS, default=SPEAKING_STATUS_PENDING)
-    
+
     class Meta:
         db_table = "proposals_proposalbase_additional_speakers"
         unique_together = ("speaker", "proposalbase")
@@ -162,14 +171,15 @@ def uuid_filename(instance, filename):
 
 
 class SupportingDocument(models.Model):
-    
+
     proposal = models.ForeignKey(ProposalBase, related_name="supporting_documents")
-    
+
     uploaded_by = models.ForeignKey(User)
     created_at = models.DateTimeField(default=datetime.datetime.now)
-    
+
     file = models.FileField(upload_to=uuid_filename)
     description = models.CharField(max_length=140)
 
     def download_url(self):
-        return reverse("proposal_document_download", args=[self.pk, os.path.basename(self.file.name).lower()])
+        return reverse("proposal_document_download",
+                       args=[self.pk, os.path.basename(self.file.name).lower()])

symposion/reviews/models.py
@@ -15,11 +15,11 @@ from symposion.schedule.models import Presentation
 
 
 class ProposalScoreExpression(object):
-    
+
     def as_sql(self, qn, connection=None):
         sql = "((3 * plus_one + plus_zero) - (minus_zero + 3 * minus_one))"
         return sql, []
-    
+
     def prepare_database_save(self, unused):
         return self
 
@@ -29,7 +29,7 @@ class Votes(object):
     PLUS_ZERO = "+0"
     MINUS_ZERO = u"−0"
     MINUS_ONE = u"−1"
-    
+
     CHOICES = [
         (PLUS_ONE, u"+1 — Good proposal and I will argue for it to be accepted."),
         (PLUS_ZERO, u"+0 — OK proposal, but I will not argue for it to be accepted."),
@@ -45,21 +45,21 @@ class ReviewAssignment(models.Model):
     AUTO_ASSIGNED_LATER = 2
 
     NUM_REVIEWERS = 3
-    
+
     ORIGIN_CHOICES = [
         (AUTO_ASSIGNED_INITIAL, "auto-assigned, initial"),
         (OPT_IN, "opted-in"),
         (AUTO_ASSIGNED_LATER, "auto-assigned, later"),
     ]
-    
+
     proposal = models.ForeignKey("proposals.ProposalBase")
     user = models.ForeignKey(User)
-    
+
     origin = models.IntegerField(choices=ORIGIN_CHOICES)
-    
+
     assigned_at = models.DateTimeField(default=datetime.now)
     opted_out = models.BooleanField()
-    
+
     @classmethod
     def create_assignments(cls, proposal, origin=AUTO_ASSIGNED_INITIAL):
         speakers = [proposal.speaker] + list(proposal.additional_speakers.all())
@@ -94,7 +94,7 @@ class ReviewAssignment(models.Model):
 class ProposalMessage(models.Model):
     proposal = models.ForeignKey("proposals.ProposalBase", related_name="messages")
     user = models.ForeignKey(User)
-    
+
     message = MarkupField()
     submitted_at = models.DateTimeField(default=datetime.now, editable=False)
 
@@ -104,24 +104,24 @@ class ProposalMessage(models.Model):
 
 class Review(models.Model):
     VOTES = VOTES
-    
+
     proposal = models.ForeignKey("proposals.ProposalBase", related_name="reviews")
     user = models.ForeignKey(User)
-    
+
     # No way to encode "-0" vs. "+0" into an IntegerField, and I don't feel
     # like some complicated encoding system.
     vote = models.CharField(max_length=2, blank=True, choices=VOTES.CHOICES)
     comment = MarkupField()
     submitted_at = models.DateTimeField(default=datetime.now, editable=False)
-    
+
     def save(self, **kwargs):
         if self.vote:
             vote, created = LatestVote.objects.get_or_create(
-                proposal = self.proposal,
-                user = self.user,
-                defaults = dict(
-                    vote = self.vote,
-                    submitted_at = self.submitted_at,
+                proposal=self.proposal,
+                user=self.user,
+                defaults=dict(
+                    vote=self.vote,
+                    submitted_at=self.submitted_at,
                 )
             )
             if not created:
@@ -130,7 +130,7 @@ class Review(models.Model):
             else:
                 self.proposal.result.update_vote(self.vote)
         super(Review, self).save(**kwargs)
-    
+
     def delete(self):
         model = self.__class__
         user_reviews = model._default_manager.filter(
@@ -152,7 +152,8 @@ class Review(models.Model):
             if self == latest:
                 # self is the latest review; revert the latest vote to the
                 # previous vote
-                previous = user_reviews.filter(submitted_at__lt=self.submitted_at).order_by("-submitted_at")[0]
+                previous = user_reviews.filter(submitted_at__lt=self.submitted_at)\
+                    .order_by("-submitted_at")[0]
                 self.proposal.result.update_vote(self.vote, previous=previous.vote, removal=True)
                 lv = LatestVote.objects.filter(proposal=self.proposal, user=self.user)
                 lv.update(
@@ -166,7 +167,7 @@ class Review(models.Model):
                 self.proposal.result.save()
         # in all cases we need to delete the review; let's do it!
         super(Review, self).delete()
-    
+
     def css_class(self):
         return {
             self.VOTES.PLUS_ONE: "plus-one",
@@ -174,7 +175,7 @@ class Review(models.Model):
             self.VOTES.MINUS_ZERO: "minus-zero",
             self.VOTES.MINUS_ONE: "minus-one",
         }[self.vote]
-    
+
     @property
     def section(self):
         return self.proposal.kind.section.slug
@@ -182,18 +183,18 @@ class Review(models.Model):
 
 class LatestVote(models.Model):
     VOTES = VOTES
-    
+
     proposal = models.ForeignKey("proposals.ProposalBase", related_name="votes")
     user = models.ForeignKey(User)
-    
+
     # No way to encode "-0" vs. "+0" into an IntegerField, and I don't feel
     # like some complicated encoding system.
     vote = models.CharField(max_length=2, choices=VOTES.CHOICES)
     submitted_at = models.DateTimeField(default=datetime.now, editable=False)
-    
+
     class Meta:
         unique_together = [("proposal", "user")]
-    
+
     def css_class(self):
         return {
             self.VOTES.PLUS_ONE: "plus-one",
@@ -223,7 +224,7 @@ class ProposalResult(models.Model):
         ("undecided", "undecided"),
         ("standby", "standby"),
     ], default="undecided")
-    
+
     @classmethod
     def full_calculate(cls):
         for proposal in ProposalBase.objects.all():
@@ -231,24 +232,24 @@ class ProposalResult(models.Model):
             result.comment_count = Review.objects.filter(proposal=proposal).count()
             result.vote_count = LatestVote.objects.filter(proposal=proposal).count()
             result.plus_one = LatestVote.objects.filter(
-                proposal = proposal,
-                vote = VOTES.PLUS_ONE
+                proposal=proposal,
+                vote=VOTES.PLUS_ONE
             ).count()
             result.plus_zero = LatestVote.objects.filter(
-                proposal = proposal,
-                vote = VOTES.PLUS_ZERO
+                proposal=proposal,
+                vote=VOTES.PLUS_ZERO
             ).count()
             result.minus_zero = LatestVote.objects.filter(
-                proposal = proposal,
-                vote = VOTES.MINUS_ZERO
+                proposal=proposal,
+                vote=VOTES.MINUS_ZERO
             ).count()
             result.minus_one = LatestVote.objects.filter(
-                proposal = proposal,
-                vote = VOTES.MINUS_ONE
+                proposal=proposal,
+                vote=VOTES.MINUS_ONE
             ).count()
             result.save()
             cls._default_manager.filter(pk=result.pk).update(score=ProposalScoreExpression())
-    
+
     def update_vote(self, vote, previous=None, removal=False):
         mapping = {
             VOTES.PLUS_ONE: "plus_one",
@@ -283,7 +284,7 @@ class Comment(models.Model):
     proposal = models.ForeignKey("proposals.ProposalBase", related_name="comments")
     commenter = models.ForeignKey(User)
     text = MarkupField()
-    
+
     # Or perhaps more accurately, can the user see this comment.
     public = models.BooleanField(choices=[
         (True, "public"),
@@ -293,7 +294,7 @@ class Comment(models.Model):
 
 
 class NotificationTemplate(models.Model):
-    
+
     label = models.CharField(max_length=100)
     from_address = models.EmailField()
     subject = models.CharField(max_length=100)
@@ -301,15 +302,16 @@ class NotificationTemplate(models.Model):
 
 
 class ResultNotification(models.Model):
-    
+
     proposal = models.ForeignKey("proposals.ProposalBase", related_name="notifications")
-    template = models.ForeignKey(NotificationTemplate, null=True, blank=True, on_delete=models.SET_NULL)
+    template = models.ForeignKey(NotificationTemplate, null=True, blank=True,
+                                 on_delete=models.SET_NULL)
     timestamp = models.DateTimeField(default=datetime.now)
     to_address = models.EmailField()
     from_address = models.EmailField()
     subject = models.CharField(max_length=100)
     body = models.TextField()
-    
+
     @property
     def email_args(self):
         return (self.subject, self.body, self.from_address, [self.to_address])
@@ -321,18 +323,18 @@ def promote_proposal(proposal):
         presentation = proposal.presentation
     else:
         presentation = Presentation(
-            title = proposal.title,
-            description = proposal.description,
-            abstract = proposal.abstract,
-            speaker = proposal.speaker,
-            section = proposal.section,
-            proposal_base = proposal,
+            title=proposal.title,
+            description=proposal.description,
+            abstract=proposal.abstract,
+            speaker=proposal.speaker,
+            section=proposal.section,
+            proposal_base=proposal,
         )
         presentation.save()
         for speaker in proposal.additional_speakers.all():
             presentation.additional_speakers.add(speaker)
             presentation.save()
-    
+
     return presentation
 
 

symposion/schedule/models.py
@@ -2,44 +2,43 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
 from markitup.fields import MarkupField
-from model_utils.managers import InheritanceManager
 
 from symposion.proposals.models import ProposalBase
 from symposion.conference.models import Section
 
 
 class Schedule(models.Model):
-    
+
     section = models.OneToOneField(Section)
     published = models.BooleanField(default=True)
     hidden = models.BooleanField("Hide schedule from overall conference view", default=False)
-    
+
     def __unicode__(self):
         return "%s Schedule" % self.section
-    
+
     class Meta:
         ordering = ["section"]
 
 
 class Day(models.Model):
-    
+
     schedule = models.ForeignKey(Schedule)
     date = models.DateField()
-    
+
     def __unicode__(self):
         return "%s" % self.date
-    
+
     class Meta:
         unique_together = [("schedule", "date")]
         ordering = ["date"]
 
 
 class Room(models.Model):
-    
+
     schedule = models.ForeignKey(Schedule)
     name = models.CharField(max_length=65)
     order = models.PositiveIntegerField()
-    
+
     def __unicode__(self):
         return self.name
 
@@ -49,22 +48,22 @@ class SlotKind(models.Model):
     A slot kind represents what kind a slot is. For example, a slot can be a
     break, lunch, or X-minute talk.
     """
-    
+
     schedule = models.ForeignKey(Schedule)
     label = models.CharField(max_length=50)
-    
+
     def __unicode__(self):
         return self.label
 
 
 class Slot(models.Model):
-    
+
     day = models.ForeignKey(Day)
     kind = models.ForeignKey(SlotKind)
     start = models.TimeField()
     end = models.TimeField()
     content_override = MarkupField(blank=True)
-    
+
     def assign(self, content):
         """
         Assign the given content to this slot and if a previous slot content
@@ -73,7 +72,7 @@ class Slot(models.Model):
         self.unassign()
         content.slot = self
         content.save()
-    
+
     def unassign(self):
         """
         Unassign the associated content with this slot.
@@ -81,7 +80,7 @@ class Slot(models.Model):
         if self.content and self.content.slot_id:
             self.content.slot = None
             self.content.save()
-    
+
     @property
     def content(self):
         """
@@ -92,14 +91,14 @@ class Slot(models.Model):
             return self.content_ptr
         except ObjectDoesNotExist:
             return None
-    
+
     @property
     def rooms(self):
         return Room.objects.filter(pk__in=self.slotroom_set.values("room"))
-    
+
     def __unicode__(self):
         return "%s %s (%s - %s)" % (self.day, self.kind, self.start, self.end)
-    
+
     class Meta:
         ordering = ["day", "start", "end"]
 
@@ -108,48 +107,49 @@ class SlotRoom(models.Model):
     """
     Links a slot with a room.
     """
-    
+
     slot = models.ForeignKey(Slot)
     room = models.ForeignKey(Room)
-    
+
     def __unicode__(self):
         return "%s %s" % (self.room, self.slot)
-    
+
     class Meta:
         unique_together = [("slot", "room")]
         ordering = ["slot", "room__order"]
 
 
 class Presentation(models.Model):
-    
+
     slot = models.OneToOneField(Slot, null=True, blank=True, related_name="content_ptr")
     title = models.CharField(max_length=100)
     description = MarkupField()
     abstract = MarkupField()
     speaker = models.ForeignKey("speakers.Speaker", related_name="presentations")
-    additional_speakers = models.ManyToManyField("speakers.Speaker", related_name="copresentations", blank=True)
+    additional_speakers = models.ManyToManyField("speakers.Speaker", related_name="copresentations",
+                                                 blank=True)
     cancelled = models.BooleanField(default=False)
     proposal_base = models.OneToOneField(ProposalBase, related_name="presentation")
     section = models.ForeignKey(Section, related_name="presentations")
-    
+
     @property
     def number(self):
         return self.proposal.number
-    
+
     @property
     def proposal(self):
         if self.proposal_base_id is None:
             return None
         return ProposalBase.objects.get_subclass(pk=self.proposal_base_id)
-    
+
     def speakers(self):
         yield self.speaker
         for speaker in self.additional_speakers.all():
             if speaker.user:
                 yield speaker
-    
+
     def __unicode__(self):
         return "#%s %s (%s)" % (self.number, self.title, self.speaker)
-    
+
     class Meta:
         ordering = ["slot"]

commit 370690e3ee8d24170664954917e8161a2726f4c0

Fix broken link in speaker biography help text.
This has been broken since 36ab6d599ffca01b83d5a93a2b5b7fa6b5bd9d70.

symposion/speakers/models.py
@@ -25,7 +25,7 @@ class Speaker(models.Model):
                                         " conference program."))
     biography = models.TextField(blank=True, help_text=_("A little bit about you.  Edit using "
                                                          "<a href='http://warpedvisions.org/projects/"
-                                                         "markdown-cheat-sheet/target='_blank'>"
+                                                         "markdown-cheat-sheet/' target='_blank'>"
                                                          "Markdown</a>."), verbose_name=_("Biography"))
     biography_html = models.TextField(blank=True)
     photo = models.ImageField(upload_to="speaker_photos", blank=True,